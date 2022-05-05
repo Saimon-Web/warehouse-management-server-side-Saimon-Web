34bbe685fece15887b6c672ceef8bcbd9f01a6e6
naim <naimctg2000@gmail.com>
2022-05-06 04:52:03 +0600

    edit some change

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-
-
-
-// function verifyjwt
 //MIDDLE WARE
 app.use(cors());
 app.use(express.json());
@@ -25,7 +21,7 @@ async function run() {
     await client.connect();
     const storeCollection = client.db('Car-delarship-project').collection('store');
 
-
+ 
     //login  time  jwt token create
     app.post('/login', async (req, res) => {
       const user = req.body;
@@ -47,7 +43,7 @@ async function run() {
     })
 
     //single data
-
+ 
     app.get('/inventory/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -68,7 +64,7 @@ async function run() {
       const result = await storeCollection.deleteOne(query)
       res.send(result)
     })
-
+ 
     //update user
     app.put('/inventory/:id', async (req, res) => {
       const id = req.params.id;
